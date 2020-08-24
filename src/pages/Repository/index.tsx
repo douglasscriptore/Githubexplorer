53c99558f4420bb0fa951291bbbd19c0a36b2a04
Douglas Scriptore <douglass@rpc.com.br>
2020-08-24 11:42:16 -0300

    first commit

---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Repository: React.FC = () => {
+  return <h1>Explore repositórips no Github</h1>;
+};
+
+export default Repository;
